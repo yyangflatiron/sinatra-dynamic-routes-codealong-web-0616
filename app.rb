9adc8bdeb5f565b3f418c9cc35db40bea10957d7
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-28 09:32:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -13,5 +13,19 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+  get '/goodbye/:name' do
+    @user_name = params[:name]
+    #resp.status = 200 #DO WE EVER NEED TO SPECIFY THE REST STATUS?
+    "Goodbye #{user_name}"
+  end
+
+  get '/multiply/:num1/:num2' do
+    @num_1 = params[:num1].to_i
+    @num_2 = params[:num2].to_i
+    @product = num_1*num_2
+    #resp.status = 200 #DO WE EVER NEED TO SPECIFY THE REST STATUS?
+    "#{product}"
+  end
+
 
 end
